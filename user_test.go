93e44332e7976188967554aecfd22e74decd9206
kimkiyong <fenderst0612@gmail.com>
2021-10-02 18:48:15 +0900

    add: e2e

--- user_test.go
+++ user_test.go
@@ -7,24 +7,47 @@ import (
 )
 
 //  referenced doc:https://github.com/gavv/httpexpect/blob/master/_examples/echo_test.go
+// TODO: add more testcase
 func UserTest(e *httpexpect.Expect) {
 	//create user
-	user1 := e.POST("/users").
+	resp := e.POST("/v1/users").
 		WithJSON(map[string]string{
 			"username": "sample_user",
 		}).Expect().Status(http.StatusCreated).JSON().Object()
 
 	// check
-	user1.ValueEqual("username", "test_user")
-	user1.Keys().ContainsOnly("ID", "username")
+	resp.ValueEqual("username", "sample_user")
+	resp.Keys().ContainsOnly("id", "username")
 
-	//get user
-	user1 = e.GET("/users").
-		WithJSON(map[string]string{
-			"username": "sample_user",
-		}).Expect().Status(http.StatusCreated).JSON().Object()
+	user1 := resp.Raw()
+
+	// get user
+	e.GET("/v1/users/" + user1["id"].(string)).
+		Expect().
+		Status(http.StatusOK).
+		JSON().Object()
 
 	// check
-	user1.ValueEqual("username", "test_user")
-	user1.Keys().ContainsOnly("ID", "username")
+	resp.ValueEqual("username", "sample_user")
+	resp.Keys().ContainsOnly("id", "username")
+
+	//get users
+	e.GET("/v1/users").
+		WithJSON(map[string]string{
+			"username": "sample_user",
+		}).Expect().Status(http.StatusOK)
+
+	// update user
+	resp = e.PATCH("/v1/users/" + user1["id"].(string)).
+		WithJSON(map[string]string{
+			"username": "updated_user",
+		}).
+		Expect().Status(http.StatusOK).JSON().Object()
+
+	resp.ValueEqual("username", "updated_user")
+	resp.Keys().ContainsOnly("id", "username")
+
+	// delete user
+	e.DELETE("/v1/users/" + user1["id"].(string)).
+		Expect().Status(http.StatusNoContent)
 }
